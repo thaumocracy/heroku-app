bf50f5896cbce9529ce66a88374779e7ce22d611
Winterstorm <thunpwnz@gmail.com>
2019-01-12 22:50:10 +0400

    F-word

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const databaseUrl = process.send.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 mongoose.Promise = global.Promise
 
 mongoose.connect(databaseUrl)
